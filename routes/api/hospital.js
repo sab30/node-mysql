aeb680277744abd022c7269b59d318188f775310
Sabarish <k.sabarish@milvik.se>
2019-08-17 18:34:34 +0530

    HA creation and HP map API done

--- routes/api/hospital.js
+++ routes/api/hospital.js
@@ -134,7 +134,7 @@ var poolReplica = db.getConnectionReplica();
             let sql= pool.format(`insert into hospitals SET ? `, values);
             [rows]= await pool.query(sql);
             if(rows){
-                res.send({results :{ id : rows.insertId , status : 'Hospital Created'} });
+                res.send({rows :{ id : rows.insertId , status : 'Hospital Created'} });
             }
             else{
                 return res.status(400).json({errors : 'Unable to create hospital'});
@@ -145,40 +145,17 @@ var poolReplica = db.getConnectionReplica();
         }
     });
 
-    /**
-     * Branch Create
-     * 
-     * @param {object} req The request object
-     * @param {object} res The response object
-     * @author Sabarish <sabarish3012@gmail.com>
-     * 
-     * @api 			{post} / Create Branch
-     * @apiName 		Create branch
-     * @apiGroup 		Hospital
-     * @apiDescription  Create and Map Branches to hospital , Roles ex: Super Admin
-     *                  
-     *
-     * @apiPermission 	Nones
-     * @access Private
-     * */
+  
 
-    router.post('/createadmin',auth,[
+    router.post('/create/admin',auth,[
         check('user_role' , 'Role is required').not().isEmpty(),
-        check('user_first_name' , 'Name is required').not().isEmpty(),
+        check('first_name' , 'Name is required').not().isEmpty(),
         check('user_email' , 'Please include a valid email').isEmail(),
         check('user_mobile' , 'user_mobile is required').not().isEmpty(),
         check('address' , 'address is required').not().isEmpty(),
         check('description' , 'description is required').not().isEmpty(),
-        check('owner_name' , 'owner_name is required').not().isEmpty(),
-        check('owner_mobile' , 'owner_mobile is required').not().isEmpty(),
-        check('city_id' , 'city_id is required').not().isEmpty(),
-        check('state_id' , 'state_id is required').not().isEmpty(),
-        check('district_id' , 'district_id is required').not().isEmpty(),
-        check('country_id' , 'country_id is required').not().isEmpty(),
-        check('license' , 'license is required').not().isEmpty(),
-        check('license_status' , 'license_status is required').not().isEmpty(),
-        check('hospital_from_date' , 'hospital_from_date is required').not().isEmpty().toDate(),
-        check('hospital_till_date' , 'hospital_till_date is required').not().isEmpty().toDate()
+        check('hospital_id' , 'hospital_id is required').not().isEmpty(),
+        check('status' , 'status is required').not().isEmpty(),
     ], async (req,res) =>{ 
 
         const errors= validationResult(req);
@@ -187,16 +164,34 @@ var poolReplica = db.getConnectionReplica();
             console.error(errors);
             return res.status(400).json({errors : errors.array()});
         }
-    
-        let {user_role,user_unique_id,user_first_name,user_password,user_mobile,user_email,address,description,owner_name,owner_mobile,city_id,state_id,district_id,country_id,
-          license,
-           license_status,
-           hospital_from_date,
-           hospital_till_date,
+
+        let {
+            user_role,
+            user_unique_id,
+            first_name,
+            last_name,
+            description,
+            user_email,
+            user_mobile,
+            hospital_id, 
+            address,
+            status
         } = req.body;
         try {
-            // Check user already exists
+
+            // Check hospital Exists;
+
             let rows, user_unique_id = null;
+            [rows] =await poolReplica.query(`select id from hospitals where id=? limit 1`,[
+                hospital_id
+            ]) ;
+
+            if(rows.length <= 0){
+                // User already exists
+                return res.status(400).json({errors : [ { message : 'Hospital dosent exist'}]});
+            }
+
+            // Check user already exists
             [rows] =await poolReplica.query(`select id from users where user_email=? limit 1`,[
                 user_email
             ]) ;
@@ -209,76 +204,100 @@ var poolReplica = db.getConnectionReplica();
             [rows] = await poolReplica.query(`select user_unique_id from users 
             where user_role=? order by id desc limit 1`,
             [ user_role ]);
-
+            
             if(rows.length > 0){
                 user_unique_id=  parseInt(rows[0].user_unique_id.split('_')[1]) + 1;
             }else{
                 user_unique_id=  config.get('uniqueStartNumber') + 1;
             }
 
-            user_unique_id = config.get('uniqueCodePrefix')[user_role] + "_" + user_unique_id.toString();
-
-            //console.log(user_unique_id);
+            const UtilsService = require('./services/UtilsService');
+            let year = moment().format('YY');
+            let nextUniqueId = UtilsService.getNextUniqueId(user_unique_id);
+            user_unique_id = config.get('uniqueCodePrefix')[user_role] + year +"_" + nextUniqueId;
 
             // Create the user
-
             let values = {
                 user_unique_id : user_unique_id,
-                user_first_name : user_first_name ? user_first_name: null,
                 user_role : user_role,
-                user_password: null, // For Hospital Creation no need for Password
                 user_mobile : user_mobile ? user_mobile : null,
                 user_email : user_email ? user_email : null,
-                address : address ? address : null,
-                description : description ? description : null,
-                owner_name : owner_name ,
-                owner_mobile : owner_mobile ,
-                city_id :city_id ,
-                state_id : state_id ,
-                district_id : district_id ,
-                country_id : country_id,
                 created_by: req.user.id
             };
 
-
             let sql= pool.format(`insert into users SET ? `, values);
             //console.log(sql);
-            const [results]= await pool.query(sql);
-
-            if(results){
+            [rows]= await pool.query(sql);
+            if(rows){
+                let user_id = rows.insertId;
                 [rows] = await poolReplica.query(`select id from roles where
                 role_code=? limit 1`,
                 [ config.get('uniqueCodePrefix')[user_role] ]);
 
                 if(rows.length > 0){
-                    let row = results[0];
                     // Map role to user
                     let values={
-                        user_id : results.insertId, 
-                        role_id : rows[0].id,
+                        user_id : rows[0].insertId,
+                        role_id : user_id,
                         created_by : req.user.id
                     }
                     let sql= pool.format(`insert into user_role SET ? `, values);
                     [rows]= await pool.query(sql);
                     if(rows){
-                        // Map License to Hospital 
-
+                        // Map HA
                         let values = {
-                            user_id : results.insertId ,
-                            hospital_name : user_first_name,
-                            user_email : user_email,
-                            hospital_description : description,
-                            license : license,
-                            license_status : license_status,
-                            hospital_from_date : hospital_from_date,
-                            hospital_till_date : hospital_till_date,
+                            user_id : user_id ,
+                            first_name : first_name ? first_name : null,
+                            last_name : last_name ? last_name : null,
+                            description : description ? description : null,
+                            hospital_id : hospital_id ? hospital_id : null,
+                            address: address,
+                            status : status,
                             created_by: req.user.id
                         };
             
-                        let sql= pool.format(`insert into hospitals SET ? `, values);
+                        let sql= pool.format(`insert into hospital_admins SET ? `, values);
                         [rows]= await pool.query(sql);
                         if(rows){
-                            res.send({results :{ user_id : results.insertId , status : 'Hospital Created'} });
+                            //Send Email 
+                        let nodemailer = require('nodemailer');
+                        let smtpTransport = require('nodemailer-smtp-transport');
+
+                        let transporter = nodemailer.createTransport(smtpTransport(config.get('smtp')));
+
+                        const payload = {
+                            id : user_id
+                        }
+                        // Email Expiry 30 min (30 * 60 * 1000)
+                        jwt.sign(
+                            payload, 
+                            config.get('jwtSecret'),
+                            {expiresIn: 3600000},
+                            (err,token) => {
+                                if (err) throw err;
+                                
+                                let mailOptions = { 
+                                    from: config.get('smtpFrom'), 
+                                    to: user_email, 
+                                    subject: 'Activate your MyPulse Account',
+                                    html: `<b>Dear User<b>`+
+                                    `<br />`
+                                    +
+                                    `Your ID is : ` + user_unique_id +
+                                    `<br />
+                                    Thanks for registering with MyPulse. Please click this button to complete your registration.`
+                                    +`<br />`+
+                                    `<a href='`+ config.get('url') +"/api/users/verifyEmail/"+ token +`'>Click Here to activate</a>`
+                                    };
+                                    //console.log(token);
+                                transporter.sendMail(mailOptions, function(error, info){ 
+                                    if (error) {
+                                        return res.status(400).json({errors : 'Unable to send email to user'});
+                                    } else {
+                                        res.send({results :{ msg : 'HA Created' } });
+                                    }
+                                });  
+                            });
                         }
                     }else{
                         return res.status(400).json({errors : 'Unable to create hospital'});
@@ -288,159 +307,9 @@ var poolReplica = db.getConnectionReplica();
             }else{
                 return res.status(400).json({errors : 'Unable to create user'});
             }
-        } catch (error) {
-            console.error(error);
-            return res.status(400).json({errors : error});
-        }
-    }
-    );
-
-    /**
-     * Hospital Admin Creation
-     * 
-     * @param {object} req The request object
-     * @param {object} res The response object
-     * @author Sabarish <sabarish3012@gmail.com>
-     * 
-     * @api 			{post} / Create or Register a Hospital admin
-     * @apiName 		Register a Hospital ADMIN
-     * @apiGroup 		Hospital
-     * @apiDescription  Create or Register a Hospital for all the Mypulse Roles ex: Super Admin
-     *                  
-     *
-     * @apiPermission 	None
-     * @access Public
-     * */
-
-    router.post('/createadmin',auth,[
-        check('user_role' , 'Role is required').not().isEmpty(),
-        check('user_first_name' , 'Name is required').not().isEmpty(),
-        check('user_email' , 'Please include a valid email').isEmail(),
-        check('user_mobile' , 'user_mobile is required').not().isEmpty(),
-        check('address' , 'address is required').not().isEmpty(),
-        check('description' , 'description is required').not().isEmpty(),
-        check('owner_name' , 'owner_name is required').not().isEmpty(),
-        check('owner_mobile' , 'owner_mobile is required').not().isEmpty(),
-        check('city_id' , 'city_id is required').not().isEmpty(),
-        check('state_id' , 'state_id is required').not().isEmpty(),
-        check('district_id' , 'district_id is required').not().isEmpty(),
-        check('country_id' , 'country_id is required').not().isEmpty(),
-        check('license' , 'license is required').not().isEmpty(),
-        check('license_status' , 'license_status is required').not().isEmpty(),
-        check('hospital_from_date' , 'hospital_from_date is required').not().isEmpty().toDate(),
-        check('hospital_till_date' , 'hospital_till_date is required').not().isEmpty().toDate()
-    ], async (req,res) =>{ 
-
-        const errors= validationResult(req);
-
-        if(!errors.isEmpty()){
+        } catch (errors) {
             console.error(errors);
-            return res.status(400).json({errors : errors.array()});
-        }
-    
-        let {user_role,user_unique_id,user_first_name,user_password,user_mobile,user_email,address,description,owner_name,owner_mobile,city_id,state_id,district_id,country_id,
-          license,
-           license_status,
-           hospital_from_date,
-           hospital_till_date,
-        } = req.body;
-        try {
-            // Check user already exists
-            let rows, user_unique_id = null;
-            [rows] =await poolReplica.query(`select id from users where user_email=? limit 1`,[
-                user_email
-            ]) ;
-
-            if(rows.length > 0){
-                // User already exists
-                return res.status(400).json({errors : [ { message : 'Email already taken'}]});
-            }
-
-            [rows] = await poolReplica.query(`select user_unique_id from users 
-            where user_role=? order by id desc limit 1`,
-            [ user_role ]);
-
-            if(rows.length > 0){
-                user_unique_id=  parseInt(rows[0].user_unique_id.split('_')[1]) + 1;
-            }else{
-                user_unique_id=  config.get('uniqueStartNumber') + 1;
-            }
-
-            user_unique_id = config.get('uniqueCodePrefix')[user_role] + "_" + user_unique_id.toString();
-
-            //console.log(user_unique_id);
-
-            // Create the user
-
-            let values = {
-                user_unique_id : user_unique_id,
-                user_first_name : user_first_name ? user_first_name: null,
-                user_role : user_role,
-                user_password: null, // For Hospital Creation no need for Password
-                user_mobile : user_mobile ? user_mobile : null,
-                user_email : user_email ? user_email : null,
-                address : address ? address : null,
-                description : description ? description : null,
-                owner_name : owner_name ,
-                owner_mobile : owner_mobile ,
-                city_id :city_id ,
-                state_id : state_id ,
-                district_id : district_id ,
-                country_id : country_id,
-                created_by: req.user.id
-            };
-
-
-            let sql= pool.format(`insert into users SET ? `, values);
-            //console.log(sql);
-            const [results]= await pool.query(sql);
-
-            if(results){
-                [rows] = await poolReplica.query(`select id from roles where
-                role_code=? limit 1`,
-                [ config.get('uniqueCodePrefix')[user_role] ]);
-
-                if(rows.length > 0){
-                    let row = results[0];
-                    // Map role to user
-                    let values={
-                        user_id : results.insertId, 
-                        role_id : rows[0].id,
-                        created_by : req.user.id
-                    }
-                    let sql= pool.format(`insert into user_role SET ? `, values);
-                    [rows]= await pool.query(sql);
-                    if(rows){
-                        // Map License to Hospital 
-
-                        let values = {
-                            user_id : results.insertId ,
-                            hospital_name : user_first_name,
-                            user_email : user_email,
-                            hospital_description : description,
-                            license : license,
-                            license_status : license_status,
-                            hospital_from_date : hospital_from_date,
-                            hospital_till_date : hospital_till_date,
-                            created_by: req.user.id
-                        };
-            
-                        let sql= pool.format(`insert into hospitals SET ? `, values);
-                        [rows]= await pool.query(sql);
-                        if(rows){
-                            res.send({results :{ user_id : results.insertId , status : 'Hospital Created'} });
-                        }
-                    }else{
-                        return res.status(400).json({errors : 'Unable to create hospital'});
-                    }
-                }
-                // Send SMS,
-            }else{
-                return res.status(400).json({errors : 'Unable to create user'});
-            }
-        } catch (error) {
-            console.error(error);
-            return res.status(400).json({errors : error});
+            return res.status(400).json({errors : errors});
         }
     }
     );
